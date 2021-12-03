981189173bdf5bed6a4bff57a8d554aae3a8e729
PierreSavatte <pierre-savatte@laposte.net>
2021-12-03 23:25:50 +0100

    Push solution for Day 2 Part 2

---
+++ day2/part2.py
@@ -0,0 +1,29 @@
+from day2.part1 import parse_file
+
+FILE = "input"
+
+
+def resolution(instructions):
+    position = {"x": 0, "z": 0, "aim": 0}
+
+    for instruction in instructions:
+        axis, value = instruction
+
+        if axis == "z":
+            axis = "aim"
+
+        position[axis] += value
+
+        if axis == "x":
+            position["z"] += value * position["aim"]
+
+    return position["x"] * position["z"]
+
+
+def challenge_resolution():
+    series = parse_file(FILE)
+    return resolution(series)
+
+
+if __name__ == "__main__":
+    print(challenge_resolution())
---
+++ tests/test_day2_part2.py
@@ -0,0 +1,16 @@
+from day2.part2 import resolution
+
+
+def test_puzzle_is_resolved():
+    instructions = [
+        ("x", 5),
+        ("z", 5),
+        ("x", 8),
+        ("z", -3),
+        ("z", 8),
+        ("x", 2),
+    ]
+
+    solution = resolution(instructions)
+
+    assert solution == 900
