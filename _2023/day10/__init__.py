0ce457e0bd7653fad47b84ffaa3439ec6310a330
PierreSavatte <pierre-savatte@laposte.net>
2023-12-10 17:45:40 +0100

    improve winding method

--- _2023/day10/__init__.py
+++ _2023/day10/__init__.py
@@ -123,6 +123,10 @@ class Map:
     def max_x(self) -> int:
         return len(self.tiles[0]) - 1
 
+    @property
+    def max_y(self) -> int:
+        return len(self.tiles[1]) - 1
+
     @classmethod
     def from_input(cls, data: str) -> "Map":
         tiles = []
@@ -231,7 +235,10 @@ class Map:
                 if (
                     tile.type == TileType.GROUND
                     and is_position_enclosed_by_loop_using_winding_number(
-                        position=tile.position, loop=loop, max_x=self.max_x
+                        position=tile.position,
+                        loop=loop,
+                        max_x=self.max_x,
+                        max_y=self.max_y,
                     )
                 ):
                     tile = Tile(position=tile.position, type=TileType.ENCLOSED)
@@ -251,7 +258,7 @@ class Map:
 
 
 def is_position_enclosed_by_loop_using_ray_casting(
-    position: Position, loop: Loop, max_x: int
+    position: Position, loop: Loop, max_x: int, max_y: int
 ) -> bool:
     starting_x = position[0]
     ending_x = max_x
@@ -272,16 +279,14 @@ def edge_function(p: Position, vertices: tuple[Position, Position]) -> float:
     return (p[0] - a[0]) * (b[1] - a[1]) - (p[1] - a[1]) * (b[0] - a[0])
 
 
-def is_position_enclosed_by_loop_using_winding_number(
+def _winding_number_x_range(
     position: Position, loop: Loop, max_x: int
 ) -> bool:
-    starting_x = position[0]
-    ending_x = max_x + 1
     loop_positions = loop.positions
 
     y = position[1]
     winding_number = 0
-    for x in range(starting_x, ending_x + 1):
+    for x in range(-1, max_x + 2):
         current_position = (x, y)
         if current_position in loop_positions:
             i = loop.get_position_index(current_position)
@@ -295,3 +300,34 @@ def is_position_enclosed_by_loop_using_winding_number(
                 winding_number -= 1
 
     return winding_number != 0
+
+
+def _winding_number_y_range(
+    position: Position, loop: Loop, max_y: int
+) -> bool:
+    loop_positions = loop.positions
+
+    x = position[0]
+    winding_number = 0
+    for y in range(-1, max_y + 2):
+        current_position = (x, y)
+        if current_position in loop_positions:
+            i = loop.get_position_index(current_position)
+            vertices = loop[i].position, loop[i + 1].position
+            edge = edge_function(position, vertices)
+            if edge == 0:
+                pass
+            elif edge > 0:
+                winding_number += 1
+            else:
+                winding_number -= 1
+
+    return winding_number != 0
+
+
+def is_position_enclosed_by_loop_using_winding_number(
+    position: Position, loop: Loop, max_x: int, max_y: int
+) -> bool:
+    enclosed_winding_x = _winding_number_x_range(position, loop, max_x)
+    enclosed_winding_y = _winding_number_y_range(position, loop, max_y)
+    return enclosed_winding_x and enclosed_winding_y
