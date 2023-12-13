30b67ff5b4d6b29374b56bab892c394b50d769fa
PierreSavatte <pierre-savatte@laposte.net>
2023-12-13 16:27:48 +0100

    add test on smudges computation

--- _2023/tests/test_day13_part2.py
+++ _2023/tests/test_day13_part2.py
@@ -1,6 +1,6 @@
 import pytest
 
-from _2023.day13 import Reflection, Pattern
+from _2023.day13 import Reflection, Pattern, compute_smudges
 from _2023.day13.part2 import compute_solution
 
 PATTERN_1 = """#.##..##.
@@ -18,6 +18,23 @@ PATTERN_2 = """#...##..#
 #####.##.
 ..##..###
 #....#..#"""
+
+
+@pytest.mark.parametrize(
+    "line_a, line_b, expected_smudges",
+    [
+        ("...#...", "...#..#", 1),
+        ("...#...", "#..#...", 1),
+        ("...#..#", "...#...", 1),
+        ("#..#...", "...#...", 1),
+        ("#..#...", "..##...", 2),
+        ("...#...", "..##...", 1),
+        ("...#...", "..#....", 2),
+        ("...#...", "######.", 5),
+    ],
+)
+def test_smudges_can_be_computed(line_a, line_b, expected_smudges):
+    assert compute_smudges(line_a, line_b) == expected_smudges
 
 
 @pytest.mark.parametrize(
