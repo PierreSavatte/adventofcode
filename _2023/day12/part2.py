53e01dbf7b46e0c0b42307803b6c7400f9b3824a
PierreSavatte <pierre-savatte@laposte.net>
2023-12-14 21:50:34 +0100

    start day12.part2

---
+++ _2023/day12/part2.py
@@ -0,0 +1,9 @@
+from _2023.load_input import load_input
+
+
+def compute_solution(data: str) -> int:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    print(compute_solution(load_input(12)))
