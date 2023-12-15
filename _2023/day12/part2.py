ee10765efa0e63f5c593b9fd80894fe8e295e934
PierreSavatte <pierre-savatte@laposte.net>
2023-12-15 20:31:05 +0100

    add day12.part2

--- _2023/day12/part2.py
+++ _2023/day12/part2.py
@@ -1,8 +1,52 @@
 from _2023.load_input import load_input
+from functools import cache
 
 
-def compute_arrangements(springs: str, groups: list[int]) -> int:
-    raise NotImplementedError()
+@cache
+def compute_arrangements(springs: str, groups: tuple[int]) -> int:
+    if not groups:
+        if "#" in springs:
+            return 0
+        else:
+            return 1
+
+    if not springs:
+        return 0
+
+    current_group = groups[0]
+    current_character = springs[0]
+
+    def dot_case():
+        return compute_arrangements(springs[1:], groups)
+
+    def hash_case():
+        next_characters = springs[:current_group]
+        next_characters = next_characters.replace("?", "#")
+
+        if next_characters != "#" * current_group:
+            return 0
+
+        if len(springs) == current_group:
+            if len(groups) == 1:
+                return 1
+            else:
+                return 0
+
+        if springs[current_group] in "?.":
+            return compute_arrangements(
+                springs[current_group + 1 :], groups[1:]
+            )
+
+        return 0
+
+    if current_character == ".":
+        return dot_case()
+    elif current_character == "#":
+        return hash_case()
+    elif current_character == "?":
+        return dot_case() + hash_case()
+
+    raise RuntimeError("Got an error")
 
 
 def compute_unfolded_arrangements(line: str) -> int:
@@ -10,7 +54,7 @@ def compute_unfolded_arrangements(line: str) -> int:
     groups = [int(group) for group in groups_txt.split(",")]
 
     unfolded_springs = "?".join([springs] * 5)
-    unfolded_groups = groups * 5
+    unfolded_groups = tuple(groups * 5)
 
     return compute_arrangements(unfolded_springs, unfolded_groups)
 
