00dd0dcba84cbf4534dc1a742a5c7f76a5e94e38
PierreSavatte <pierre-savatte@laposte.net>
2023-12-15 08:43:51 +0100

    try day12.part2

--- _2023/day12/part2.py
+++ _2023/day12/part2.py
@@ -1,8 +1,15 @@
 from _2023.load_input import load_input
 
 
-def compute_solution(data: str) -> int:
+def compute_arrangements(line: str) -> int:
     raise NotImplementedError()
+
+
+def compute_solution(data: str) -> int:
+    total_possible_arrangements = 0
+    for line in data.splitlines():
+        total_possible_arrangements += compute_arrangements(line)
+    return total_possible_arrangements
 
 
 if __name__ == "__main__":
--- _2023/tests/test_day12_part2.py
+++ _2023/tests/test_day12_part2.py
@@ -1,4 +1,23 @@
-from _2023.day12.part2 import compute_solution
+import pytest
+
+from _2023.day12.part2 import compute_solution, compute_arrangements
+
+
+@pytest.mark.parametrize(
+    "line, expected_number_of_arrangements",
+    [
+        ("???.### 1,1,3", 1),
+        (".??..??...?##. 1,1,3", 16384),
+        ("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+        ("????.#...#... 4,1,1", 16),
+        ("????.######..#####. 1,6,5", 2500),
+        ("?###???????? 3,2,1", 506250),
+    ],
+)
+def test_number_of_arrangements_can_be_computed_from_line(
+    line, expected_number_of_arrangements
+):
+    assert compute_arrangements(line) == expected_number_of_arrangements
 
 
 def test_solution_can_be_computed(get_data):
