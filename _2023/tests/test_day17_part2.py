e90989e91e33b1004075454f4e450ac0eff4e75a
PierreSavatte <pierre-savatte@laposte.net>
2023-12-21 09:01:50 +0100

    add test for path computing

--- _2023/tests/test_day17_part2.py
+++ _2023/tests/test_day17_part2.py
@@ -1,6 +1,31 @@
 import pytest
 
+from _2023.day17 import Map, build_solution_tiles, CrucibleType
 from _2023.day17.part2 import compute_solution
+from _2023.day17.pathfinding import a_star
+
+EXPECTED_SHORTEST_PATH = """2>>>>>>>>1323
+32154535v5623
+32552456v4254
+34465858v5452
+45466578v>>>>
+143859879845v
+445787698776v
+363787797965v
+465496798688v
+456467998645v
+122468686556v
+254654888773v
+432267465553v"""
+
+
+def test_shortest_path_can_be_computed(get_data):
+    map = Map.from_data(
+        get_data("test_file_day17_part1"), crucible_type=CrucibleType.ULTRA
+    )
+    shortest_route = a_star(map=map)
+
+    assert build_solution_tiles(map, shortest_route) == EXPECTED_SHORTEST_PATH
 
 
 @pytest.mark.parametrize(
