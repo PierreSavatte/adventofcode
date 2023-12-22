b3dadc97a1242953b0b4b5ad633301757558ac1a
PierreSavatte <pierre-savatte@laposte.net>
2023-12-22 18:12:14 +0100

    day18.part1: try day18.part1

---
+++ _2023/day18/part1.py
@@ -0,0 +1,14 @@
+from _2023.day18 import DigPlan, Plan
+from _2023.load_input import load_input
+
+
+def compute_solution(data: str) -> int:
+    dig_plan = DigPlan.from_data(data)
+    plan = Plan.from_dig_plan(dig_plan)
+    fully_dug_plan = plan.compute_fully_dug_plan()
+    return len(fully_dug_plan.dug_cells)
+
+
+if __name__ == "__main__":
+    # solution < 13620
+    print(compute_solution(load_input(18)))
--- _2023/tests/test_day18_part1.py
+++ _2023/tests/test_day18_part1.py
@@ -1,6 +1,7 @@
 import pytest
 
 from _2023.day18 import DigPlan, Order, Direction, Plan
+from _2023.day18.part1 import compute_solution
 
 EXPECTED_PLAN = """#######
 #.....#
@@ -65,3 +66,7 @@ def test_plan_can_be_output_as_string(plan):
 def test_enclosing_plan_can_be_computed(plan):
     fully_dug_plan = plan.compute_fully_dug_plan()
     assert fully_dug_plan.as_string() == EXPECTED_FULLY_DUG_PLAN
+
+
+def test_solution_can_be_computed(get_data):
+    assert compute_solution(get_data("test_file_day18")) == 62
