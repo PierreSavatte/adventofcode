110cf3009be3ece3c80782475a23c82ee9e5e779
PierreSavatte <pierre-savatte@laposte.net>
2024-12-02 08:12:47 +0100

    Improve naming

--- _2024/day2/part1.py
+++ _2024/day2/part1.py
@@ -7,25 +7,25 @@ def are_adjacent_levels_safe(a: int, b: int) -> bool:
     return 1 <= diff <= 3
 
 
-def _are_levels_strictly_increasing(levels: list[int]) -> bool:
-    return all(a < b for a, b in zip(levels, levels[1:]))
+def _are_levels_strictly_increasing(report: list[int]) -> bool:
+    return all(a < b for a, b in zip(report, report[1:]))
 
 
-def _are_levels_strictly_decreasing(levels: list[int]) -> bool:
-    return all(a > b for a, b in zip(levels, levels[1:]))
+def _are_levels_strictly_decreasing(report: list[int]) -> bool:
+    return all(a > b for a, b in zip(report, report[1:]))
 
 
-def are_levels_strictly_monotonic(levels: list[int]) -> bool:
-    strictly_increasing = _are_levels_strictly_increasing(levels)
-    strictly_decreasing = _are_levels_strictly_decreasing(levels)
+def are_levels_strictly_monotonic(report: list[int]) -> bool:
+    strictly_increasing = _are_levels_strictly_increasing(report)
+    strictly_decreasing = _are_levels_strictly_decreasing(report)
     return strictly_increasing or strictly_decreasing
 
 
-def is_safe(levels: list[int]) -> bool:
+def is_safe(report: list[int]) -> bool:
     all_adjacent_levels_are_safe = all(
-        are_adjacent_levels_safe(a, b) for a, b in zip(levels, levels[1:])
+        are_adjacent_levels_safe(a, b) for a, b in zip(report, report[1:])
     )
-    levels_strictly_are_monotonic = are_levels_strictly_monotonic(levels)
+    levels_strictly_are_monotonic = are_levels_strictly_monotonic(report)
     return all_adjacent_levels_are_safe and levels_strictly_are_monotonic
 
 
