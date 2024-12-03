0d0b583a8761a1b118a6c11be951cca1ecc79320
PierreSavatte <pierre-savatte@laposte.net>
2024-12-03 12:40:04 +0100

    Improve readability

--- _2024/day3/part2.py
+++ _2024/day3/part2.py
@@ -5,7 +5,7 @@ from _2024.load_input import load_input
 
 PART_TO_REMOVE_REGEX = r"don't\(\).*?do\(\)"
 
-END_OF_STRING_WITH_DONT = r"don't\(\).*?$"
+END_OF_STRING_WITH_DONT = r"don't\(\)(?!.*don't\(\)).*\Z"
 
 
 def remove_ignored_part_of_input(data: str) -> str:
@@ -13,12 +13,17 @@ def remove_ignored_part_of_input(data: str) -> str:
     split_data = re.split(PART_TO_REMOVE_REGEX, data)
     new_data = "".join(split_data)
 
-    split_data = re.split(END_OF_STRING_WITH_DONT, new_data)
+    match = re.search(END_OF_STRING_WITH_DONT, new_data)
+    if match:
+        end_with_dont = match.group()
+        if not "do()" in end_with_dont:
+            split_data = re.split(END_OF_STRING_WITH_DONT, new_data)
 
     return "".join(split_data)
 
 
 if __name__ == "__main__":
     input_data = load_input(3)
-    groups = parse_input(remove_ignored_part_of_input(input_data))
+    new_input_data = remove_ignored_part_of_input(input_data)
+    groups = parse_input(new_input_data)
     print(compute_solution(groups))
