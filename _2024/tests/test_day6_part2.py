ff056a56e8c3881e7c88ea514fa475d02c6c2473
PierreSavatte <pierre-savatte@laposte.net>
2024-12-07 14:12:27 +0100

    2024.day6.part2: Fix line declaration in test

--- _2024/tests/test_day6_part2.py
+++ _2024/tests/test_day6_part2.py
@@ -5,9 +5,21 @@ from _2024.day6 import Line
 @pytest.mark.parametrize(
     "line_a, line_b, intersection_point",
     [
-        (Line((0, 0), (10, 0)), Line((0, 0), (0, 10)), (0, 0)),
-        (Line((0, 0), (10, 0)), Line((5, 0), (5, 10)), (5, 0)),
-        (Line((0, 0), (10, 0)), Line((1, 1), (1, 10)), (1, 0)),
+        (
+            Line((0, 0), (10, 0), Direction.UP),
+            Line((0, 0), (0, 10), Direction.UP),
+            (0, 0),
+        ),
+        (
+            Line((0, 0), (10, 0), Direction.UP),
+            Line((5, 0), (5, 10), Direction.UP),
+            (5, 0),
+        ),
+        (
+            Line((0, 0), (10, 0), Direction.UP),
+            Line((1, 1), (1, 10), Direction.UP),
+            (1, 0),
+        ),
     ],
 )
 def test_intersection_can_be_evaluated_between_two_lines(
@@ -20,8 +32,14 @@ def test_intersection_can_be_evaluated_between_two_lines(
 @pytest.mark.parametrize(
     "line_a, line_b",
     [
-        (Line((0, 0), (10, 0)), Line((0, 1), (10, 1))),
-        (Line((0, 0), (0, 10)), Line((1, 0), (1, 10))),
+        (
+            Line((0, 0), (10, 0), Direction.UP),
+            Line((0, 1), (10, 1), Direction.UP),
+        ),
+        (
+            Line((0, 0), (0, 10), Direction.UP),
+            Line((1, 0), (1, 10), Direction.UP),
+        ),
     ],
 )
 def test_none_is_evaluated_when_no_intersection_exists_between_two_lines(
