f91423f43dc557d11cc17669cdaaf533f26a5acf
PierreSavatte <pierre-savatte@laposte.net>
2024-12-11 07:46:53 +0100

    Rename functions

--- _2024/day11/part2.py
+++ _2024/day11/part2.py
@@ -5,36 +5,36 @@ from _2024.load_input import load_input
 
 
 @cache
-def fast_change_stone(stone: int, nb_change: int) -> int:
+def change_stone_fast(stone: int, nb_change: int) -> int:
     if nb_change == 0:
         return 1
 
     new_nb_change = nb_change - 1
 
     if stone == 0:
-        return fast_change_stone(1, nb_change=new_nb_change)
+        return change_stone_fast(1, nb_change=new_nb_change)
 
     stone_str = str(stone)
     if len(stone_str) % 2 == 0:
         i = len(stone_str) // 2
         a, b = stone_str[:i], stone_str[i:]
-        return fast_change_stone(
+        return change_stone_fast(
             int(a), nb_change=new_nb_change
-        ) + fast_change_stone(int(b), nb_change=new_nb_change)
+        ) + change_stone_fast(int(b), nb_change=new_nb_change)
 
     new_stone = stone * 2024
-    return fast_change_stone(new_stone, nb_change=new_nb_change)
+    return change_stone_fast(new_stone, nb_change=new_nb_change)
 
 
-def fast_change(stones: list[int], nb_change: int) -> int:
+def change_fast(stones: list[int], nb_change: int) -> int:
     total = 0
     for stone in stones:
-        total += fast_change_stone(stone, nb_change)
+        total += change_stone_fast(stone, nb_change)
     return total
 
 
 def compute_solution(stones: list[int], nb_change: int) -> int:
-    return fast_change(stones, nb_change)
+    return change_fast(stones, nb_change)
 
 
 def main():
