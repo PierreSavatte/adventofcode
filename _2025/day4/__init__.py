9e2dc9914678e1d14c08e9c1b00eb1cc9f7fec94
PierreSavatte <pierre-savatte@laposte.net>
2025-12-05 08:30:49 +0100

    Start the implementation of 2025.day4.part1

---
+++ _2025/day4/__init__.py
@@ -0,0 +1,92 @@
+from pprint import pprint
+
+from _2025.load_input import load_input
+
+MAP = list[list[str]]
+NEIGHBOURS_COUNT = list[list[int]]
+
+
+def parse_map(input: str) -> MAP:
+    map = []
+    for row in input.strip().splitlines():
+        map.append(list(row))
+    return map
+
+
+def get_neighbours(map: MAP, x: int, y: int) -> list[str]:
+    neighbours = []
+    for y_ in range(y - 1, y + 2):
+        if y_ < 0 or y_ >= len(map):
+            continue
+
+        for x_ in range(x - 1, x + 2):
+            if x_ < 0 or x_ >= len(map[0]):
+                continue
+            if x_ == x and y_ == y:
+                continue
+
+            neighbours.append(map[y_][x_])
+
+    return neighbours
+
+
+def compute_neighbour_count(map: MAP) -> NEIGHBOURS_COUNT:
+    new_map = []
+    for y in range(len(map)):
+        new_row = []
+        for x in range(len(map[y])):
+            if map[y][x] == ".":
+                count = None
+            else:
+                neighbours = get_neighbours(map, x, y)
+                count = sum(
+                    [1 for neighbour in neighbours if neighbour == "@"]
+                )
+            new_row.append(count)
+        new_map.append(new_row)
+    return new_map
+
+
+def flatten_2d_map(map: NEIGHBOURS_COUNT) -> list[int]:
+    flatten = []
+    for row in map:
+        flatten.extend(row)
+    return flatten
+
+
+def mark(map: MAP) -> list[str]:
+    neighbour_count = compute_neighbour_count(map)
+    new_map = []
+    for y in range(len(map)):
+        new_row = []
+        for x in range(len(map[y])):
+            nb_neighbours = neighbour_count[y][x]
+            if nb_neighbours is None:
+                cell = "."
+            else:
+                if nb_neighbours < 4:
+                    cell = "x"
+                else:
+                    cell = "@"
+
+            new_row.append(cell)
+        new_map.append("".join(new_row) + "\n")
+    return new_map
+
+
+def compute_solution(map: MAP) -> int:
+    neighbours_count = flatten_2d_map(compute_neighbour_count(map))
+    return sum(1 for count in neighbours_count if count and count < 4)
+
+
+def main():
+    map = parse_map(load_input(4))
+    print(compute_solution(map))
+
+
+if __name__ == "__main__":
+    # > 1390
+    # < 3699
+    # != 2311
+    # != 3471
+    main()
---
+++ _2025/tests/test_day4_part1.py
@@ -0,0 +1,116 @@
+import pytest
+from _2025.day4 import (
+    compute_neighbour_count,
+    compute_solution,
+    get_neighbours,
+    mark,
+    parse_map,
+)
+
+INPUT = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
+MAP = [
+    [".", ".", "@", "@", ".", "@", "@", "@", "@", "."],
+    ["@", "@", "@", ".", "@", ".", "@", ".", "@", "@"],
+    ["@", "@", "@", "@", "@", ".", "@", ".", "@", "@"],
+    ["@", ".", "@", "@", "@", "@", ".", ".", "@", "."],
+    ["@", "@", ".", "@", "@", "@", "@", ".", "@", "@"],
+    [".", "@", "@", "@", "@", "@", "@", "@", ".", "@"],
+    [".", "@", ".", "@", ".", "@", ".", "@", "@", "@"],
+    ["@", ".", "@", "@", "@", ".", "@", "@", "@", "@"],
+    [".", "@", "@", "@", "@", "@", "@", "@", "@", "."],
+    ["@", ".", "@", ".", "@", "@", "@", ".", "@", "."],
+]
+
+
+def test_map_can_be_parsed():
+    assert parse_map(INPUT) == MAP
+
+
+def test_neighbours_can_be_fetched():
+    map = [
+        [".", ".", "@"],
+        ["@", "@", "@"],
+        ["@", "@", "@"],
+    ]
+
+    assert get_neighbours(map, x=0, y=0) == [".", "@", "@"]
+    assert get_neighbours(map, x=2, y=0) == [".", "@", "@"]
+    assert get_neighbours(map, x=1, y=1) == [
+        ".",
+        ".",
+        "@",
+        "@",
+        "@",
+        "@",
+        "@",
+        "@",
+    ]
+    assert get_neighbours(map, x=2, y=2) == ["@", "@", "@"]
+
+
+@pytest.mark.parametrize(
+    "map, expected_output",
+    [
+        (
+            [
+                [".", ".", "@"],
+                ["@", "@", "@"],
+                ["@", "@", "@"],
+            ],
+            [
+                [None, None, 2],
+                [3, 6, 4],
+                [3, 5, 3],
+            ],
+        ),
+        (
+            MAP,
+            [
+                [None, None, 3, 3, None, 3, 3, 4, 3, None],
+                [3, 6, 6, None, 4, None, 4, None, 5, 4],
+                [4, 7, 6, 7, 5, None, 2, None, 4, 4],
+                [4, None, 6, 7, 7, 6, None, None, 4, None],
+                [3, 5, None, 7, 8, 7, 5, None, 4, 3],
+                [None, 4, 6, 5, 7, 6, 6, 5, None, 4],
+                [None, 4, None, 6, None, 5, None, 6, 7, 4],
+                [2, None, 6, 6, 6, None, 6, 7, 7, 4],
+                [None, 5, 5, 7, 6, 7, 6, 7, 5, None],
+                [1, None, 3, None, 4, 5, 4, None, 2, None],
+            ],
+        ),
+    ],
+)
+def test_nb_adjacent_roll_of_papers_can_be_computed(map, expected_output):
+    assert compute_neighbour_count(map) == expected_output
+
+
+def test_map_can_be_marked_with_position_that_has_not_too_much_neighbours():
+    expected_marked_map = [
+        "..xx.xx@x.\n",
+        "x@@.@.@.@@\n",
+        "@@@@@.x.@@\n",
+        "@.@@@@..@.\n",
+        "x@.@@@@.@x\n",
+        ".@@@@@@@.@\n",
+        ".@.@.@.@@@\n",
+        "x.@@@.@@@@\n",
+        ".@@@@@@@@.\n",
+        "x.x.@@@.x.\n",
+    ]
+
+    assert mark(MAP) == expected_marked_map
+
+
+def test_solution_can_be_computed():
+    assert compute_solution(MAP) == 13
