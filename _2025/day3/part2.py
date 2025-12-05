ca0f64669b674a93f132db14a1d5bd44a7bc305e
PierreSavatte <pierre-savatte@laposte.net>
2025-12-05 08:01:30 +0100

    Implement 2025.day3.part2

--- _2025/day3/part2.py
+++ _2025/day3/part2.py
@@ -1,17 +1,38 @@
-from itertools import combinations
-
 from _2025.day3 import BaseBanks
 from _2025.load_input import load_input
 
 
+def pop_from_string(string: str, index: int) -> str:
+    return string[:index] + string[index + 1 :]
+
+
 class LargeBanks(BaseBanks):
     def get_bank_max_joltage(self, bank: str) -> int:
-        max = 0
-        for combination in combinations(bank, 12):
-            current = int("".join(combination))
-            if current > max:
-                max = current
-        return max
+        nb_available_batteries = 12
+        nb_batteries = len(bank)
+        nb_banks_to_remove = nb_batteries - nb_available_batteries
+
+        while nb_banks_to_remove > 0:
+            has_removed_a_battery = False
+            i = 0
+            while i < len(bank) - 1:
+                if int(bank[i]) < int(bank[i + 1]):
+                    bank = pop_from_string(bank, i)
+                    nb_banks_to_remove -= 1
+                    has_removed_a_battery = True
+                    break
+                else:
+                    i += 1
+
+            while not has_removed_a_battery:
+                for j in range(10):
+                    if (index := bank.find(str(j))) != -1:
+                        bank = pop_from_string(bank, index)
+                        nb_banks_to_remove -= 1
+                        has_removed_a_battery = True
+                        break
+
+        return int(bank)
 
 
 def main():
